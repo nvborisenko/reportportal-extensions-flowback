d577950e230449003630e090c0a6891ce50f7a21
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2020-06-19 16:31:20 +0300

    qwe

---
+++ src/ReportPortal.Extensions.Insider.Task/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReportPortal.Extensions.Insider.Task
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            throw new Exception("Hi from TOOL!@!!!!");
+        }
+    }
+}
